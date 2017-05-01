88f71950ed0c2d8d35cb2aceeba19d749752d7c6
Deepika <deepikakalani@gmail.com>
2017-05-01 15:15:07 -0700

    Using Volley to make API call

---
+++ app/src/main/java/com/example/cmpe277/sjsu/socialmediaapp/Model/EmailService.java
@@ -0,0 +1,8 @@
+package com.example.cmpe277.sjsu.socialmediaapp.Model;
+
+/**
+ * Created by deepika on 5/1/17.
+ */
+
+public class EmailService {
+}
--- app/src/main/java/com/example/cmpe277/sjsu/socialmediaapp/Register.java
+++ app/src/main/java/com/example/cmpe277/sjsu/socialmediaapp/Register.java
@@ -2,12 +2,19 @@ package com.example.cmpe277.sjsu.socialmediaapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import com.squareup.okhttp.*;
-
 import java.io.IOException;
+
+import com.android.volley.*;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.okhttp.*;
+import com.squareup.okhttp.Request;
+
 
 /**
  * Created by deepika on 4/25/17.
@@ -18,6 +25,10 @@ public class Register extends Activity{
     EditText useremail, userpassword;
     Button rbutton;
     String email, password;
+    private static final String TAG = Register.class.getName();
+    private RequestQueue mRequestQueue;
+    private StringRequest stringRequest;
+    private String url = "http://www.mocky.io/v2/5907a8ce10000042179bd83f";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +37,32 @@ public class Register extends Activity{
         rbutton = (Button)findViewById(R.id.rsubmit);
         useremail = (EditText)findViewById(R.id.temail);
         userpassword = (EditText)findViewById(R.id.tpassword);
+        //verificationMail();
 
         rbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 //email = useremail.getText().toString();
                 //password = userpassword.getText().toString();
-                verificationMail();
+                //verificationMail();
+                sendRequest();
+
+
             }
         });
 
     }
 
-    private void verificationMail() {
-
+  /* private void verificationMail() {
+        //initialize the httpClient
         OkHttpClient client = new OkHttpClient();
 
+        //initiaize a request
         Request request = new Request.Builder()
                 .url("https://us15.api.mailchimp.com/3.0/lists/7a5fc07dd3")
                 .build();
 
+        //execute the request
         Response response = null;
         try {
             response = client.newCall(request).execute();
@@ -53,8 +70,39 @@ public class Register extends Activity{
             e.printStackTrace();
         }
 
-        System.out.println(response);
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.i("TAG", e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                Log.i("TAG", response.body().string());
+            }
+        });
+        //System.out.println(response);
+    }*/
+
+    public void sendRequest(){
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        stringRequest = new StringRequest(com.android.volley.Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i(TAG, response.toString());
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Log.i(TAG, "Error: " + error.toString());
+            }
+        });
+
+        mRequestQueue.add(stringRequest);
     }
+
 
 
 }
